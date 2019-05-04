3d264d144dc17181755e1633fa21e249364d8056
Ben Senescu <44480372+bensenescu@users.noreply.github.com>
2019-05-04 16:49:08 -0400

    Seperated sketch from pong game and changed pong into a class

--- pong.js
+++ pong.js
@@ -1,166 +1,173 @@
-//game properties
-var gameWidth = 750;
-var gameHeight = 600;
-var isGameStarted = false;
-var isGameOver = false;
-var isRoundOver;
-var isFatalCollision;
-var lives = 3;
-var score = 0;
+class Pong {
+  constructor(gameWidth, gameHeight, xBallSpeed, yBallSpeed, lives, score) {
+    //console.log("constructor")
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.lives = lives;
+    this.score = score;
 
-//ball properties
-var xBall;
-var yBall;
-var diameter;
-var xBallChange;
-var yBallChange;
+    //console.log("score:" + this.score)
+    this.xBallChange = xBallSpeed;
+    this.yBallChange = yBallSpeed;
 
-// paddle properties
-
-var xPaddle;
-var yPaddle;
-var paddleWidth;
-var paddleHeight;
-
-function setup() {
-  createCanvas(gameWidth, gameHeight);
-}
-
-function draw() {
-  background(220);
-
-  if (!isGameStarted) {
-    intializeGame();
-  }
-
-  //draw lives
-  fill(255, 0, 0);
-  textSize(30);
-  text("Lives: " + lives, 10, 30);
-
-  //draw score
-  fill(0, 255, 255);
-  textSize(30);
-  text("Score: " + score, gameWidth - 140, 30);
-
-  if (!isFatalCollision) {
-    detectCollision();
-  }
+    this.xBall = (Math.floor(Math.random() * 300) + 50);
+    this.yBall = 50;
+    this.diameter = 50;   
   
-  //ball movement
-  yBall += yBallChange;
-  xBall += xBallChange;
+    this.paddleWidth = 100;
+    this.paddleHeight = 25;
+    this.xPaddle = gameWidth / 2 - this.paddleWidth / 2;
+    this.yPaddle = gameHeight - this.paddleHeight - 5;
+
+    this.isGameOver = false;
+    this.isRoundOver = false;
+    this.isFatalCollision = false;
+    this.isGameStarted = true; 
+  }
+
+  playPong() {    
+    //console.log("playPong")
+    //draw lives
+    fill(255, 0, 0);
+    textSize(30);
+    text("Lives: " + this.lives, 10, 30);
+
+    //draw score
+    fill(0, 255, 255);
+    textSize(30);
+    text("Score: " + this.score, this.gameWidth - 140, 30);
+
+    if (!this.isFatalCollision) {
+      this.detectCollision();
+    }
+  
+    //ball movement
+    this.yBall += this.yBallChange;
+    this.xBall += this.xBallChange;
  
-  //draw ball at current spot
-  drawBall(xBall, yBall, diameter);
+    //draw ball at current spot
+    this.drawBall(this.xBall, this.yBall, this.diameter);
   
-  //draw the rectangle at the given spot
-  drawRectangle(xPaddle, yPaddle, paddleWidth, paddleHeight);
+    //draw the rectangle at the given spot
+    this.drawRectangle(this.xPaddle, this.yPaddle, 
+      this.paddleWidth, this.paddleHeight);
 
-  detectRoundOver();
+    this.detectRoundOver();
 
-  if (isGameOver) {
-
-  } else if (isRoundOver) {
-    restartRound();
+    //console.log(this.isRoundOver)
+    if (this.isRoundOver) {
+      console.log("isRoundOver was true")
+      this.restartRound();
+    }
   }
-}
 
-//initialize the state of a new game or round
-function intializeGame() {
-  console.log("intializeGame");
-  xBallChange = 5;
-  yBallChange = 5;
+  drawBall(x, y, d) {
+    //console.log("drawBall")
+    //sets color
+    fill(255, 0, 255);
+    //prevents the ball from having an outline
+    noStroke();
+    //places the ball at x,y with a size of w,h
+    ellipse(x, y, d, d);
+  } 
 
-  xBall = (Math.floor(Math.random() * 300) + 50);
-  yBall = 50;
-  diameter = 50;   
-  drawBall(xBall, yBall, diameter);
+  drawRectangle(x, y, w, h) {
+    //console.log("drawRect")
+    fill(0, 255, 255);
+    noStroke();
+    rect(x, y, w, h);
+  }
 
-  paddleWidth = 100;
-  paddleHeight = 25;
-  xPaddle = gameWidth / 2 - paddleWidth / 2;
-  yPaddle = gameHeight - paddleHeight - 5;
-  drawRectangle(xPaddle, yPaddle, paddleWidth, paddleHeight);
-
-  isGameOver = false;
-  isRoundOver = false;
-  isFatalCollision = false;
-  isGameStarted = true;
-}
-
-function drawBall(x, y, d) {
-  //sets color
-  fill(255, 0, 255);
-  //prevents the ball from having an outline
-  noStroke();
-  //places the ball at x,y with a size of w,h
-  ellipse(x, y, d, d);
-}
-
-function drawRectangle(x, y, w, h) {
-  fill(0, 255, 255);
-  noStroke();
-  rect(x, y, w, h);
-}
-
-//detects if the ball collides with a wall or the paddle
-function detectCollision() {
-  //detects collision with top wall
-  if (yBall < diameter / 2 || yBall > gameHeight - diameter / 2) {
-    yBallChange *= -1;
+  //detects if the ball collides with a wall or the paddle
+  detectCollision() {
+    //console.log("detectCollision")
+    //detects collision with top wall
+    if (this.yBall < this.diameter / 2 
+      || this.yBall > this.gameHeight - this.diameter / 2) {
+        this.yBallChange *= -1;
     
-    //detects collisions with either side wall
-  } else if (xBall < diameter / 2 || xBall > gameWidth - diameter / 2) {
-    xBallChange *= -1;
+      //detects collisions with either side wall
+    } else if (this.xBall < this.diameter / 2 
+      || this.xBall > this.gameWidth - this.diameter / 2) {
+        this.xBallChange *= -1;
 
-    //detects collsion with paddle
-  } else if (yBall >= (yPaddle - diameter / 2) && (xBall > xPaddle && (xBall  < xPaddle + paddleWidth))) {
-    yBallChange *= -1;
-    score++;
+      //detects collsion with paddle
+    } else if (this.yBall >= (this.yPaddle - this.diameter / 2) 
+      && (this.xBall > this.xPaddle 
+        && (this.xBall  < this.xPaddle + this.paddleWidth))) {
+          this.yBallChange *= -1;
+          this.score++;
 
-    //TODO: fix this so that it doesnt just stop the game from working when it hits the side
-    //checks if it hit the side of the paddle so that it doesnt just float through the paddle
-    //and hit the bottom.
-  } else if (yBall >= (yPaddle - diameter / 2) && (xBall > xPaddle - diameter /2 && (xBall  < xPaddle + paddleWidth + diameter /2))) {
-    xBallChange *= -1;
-    isFatalCollision = true;
-  }
-}
-
-//detects if the ball hit the bottom wall
-//changes the round state to over
-function detectRoundOver() {
-  if (yBall + diameter / 2 > gameHeight) {
-    isRoundOver = true;
-  }
-}
-
-//moves the paddle left or right based off of key movements
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    if(xPaddle < 50) {
-      xPaddle = 0;
-    } else {
-      xPaddle -= 50;
+      //TODO: fix this so that it doesnt just stop the game from working when it hits the side
+      //checks if it hit the side of the paddle so that it doesnt just float through the paddle
+      //and hit the bottom.
+    } else if (this.yBall >= (this.yPaddle - this.diameter / 2) 
+      && (this.xBall > this.xPaddle - this.diameter /2 
+        && (this.xBall  < this.xPaddle + this.paddleWidth + this.diameter /2))) {
+      this.xBallChange *= -1;
+      this.isFatalCollision = true;
     }
-  } else if (keyCode === RIGHT_ARROW) {
-    if (xPaddle > gameWidth - paddleWidth - 50) {
-      xPaddle = gameWidth - paddleWidth;
-    } else {
-      xPaddle += 50;
+  } 
+
+  //detects if the ball hit the bottom wall
+  //changes the round state to over
+  detectRoundOver() {
+    //console.log("detectRoundOver")
+    if (this.yBall + this.diameter / 2 > this.gameHeight) {
+      this.isRoundOver = true;
     }
   }
-}
 
-//restart the round through initialize game and take away a life
-//if the player is out of lives, change the isGameOver state to true
-function restartRound() {
-  if (lives === 0){
-    isGameOver = true;
-  } else {
-    lives--;
-    intializeGame();
+  //moves the paddle left or right based off of key movements
+  movePaddle(keyCode) {
+    //console.log("movePaddle")
+    if (keyCode === LEFT_ARROW) {
+      if(this.xPaddle < 50) {
+        this.xPaddle = 0;
+      } else {
+        this.xPaddle -= 50;
+      }
+    } else if (keyCode === RIGHT_ARROW) {
+      if (this.xPaddle > this.gameWidth - this.paddleWidth - 50) {
+        this.xPaddle = this.gameWidth - this.paddleWidth;
+      } else {
+        this.xPaddle += 50;
+      }
+    }
+  }
+
+  //restart the round through initialize game and take away a life
+  //if the player is out of lives, change the isGameOver state to true
+  restartRound() {
+    console.log("restartRound")
+    if (this.lives === 0){
+      this.isGameOver = true;
+    } else {
+      this.lives--;
+  
+      //reset speeds
+      this.xBallChange = Math.abs(this.xBallChange);
+      this.yBallChange = Math.abs(this.yBallChange);
+
+      //reset ball position
+      this.xBall = (Math.floor(Math.random() * 300) + 50);
+      this.yBall = 50; 
+
+      //reset paddle position
+      this.xPaddle = this.gameWidth / 2 - this.paddleWidth / 2;
+      this.yPaddle = this.gameHeight - this.paddleHeight - 5;
+
+      //reset game states
+      this.isRoundOver = false;
+      this.isFatalCollision = false; 
+    }
+  }
+
+  getIsGameOver() {
+    return this.isGameOver;
+  }
+
+  getScore() {
+    return this.score;
   }
 }
-
--- sketch.js
+++ sketch.js
@@ -1,8 +1,38 @@
+var gameWidth = 750;
+var gameHeight = 600;
+var isGameStarted = false;
+var pong;
+
 
 function setup() {
     createCanvas(gameWidth, gameHeight);
 }
 
 function draw() {
-    
+    background(220);
+    if (!isGameStarted) {
+        startButton = createButton('Start Game!');
+        startButton.position(gameWidth / 2, gameHeight /2);
+        startButton.mousePressed(startGame);
+    } else if (pong.getIsGameOver()) {
+        fill(255, 0, 0);
+        textSize(60);
+        text("Game Over!", gameWidth / 2 - 175, gameHeight / 2);
+        
+        fill(0, 255, 255);
+        textSize(40);
+        text("Score: " + pong.getScore(), gameWidth / 2 - 90, gameHeight / 2 + 40);
+    } else {
+        pong.playPong();
+    }
+}
+
+function keyPressed() {
+    pong.movePaddle(keyCode);
+}
+
+function startGame() {
+    startButton.hide();
+    pong = new Pong(gameWidth, gameHeight, 5, 5, 3, 0);
+    isGameStarted = true;
 }
