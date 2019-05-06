af73e011e603960278112d5d2171b8d5407ed3b6
Ben Senescu <44480372+bensenescu@users.noreply.github.com>
2019-05-06 19:07:37 -0400

    Created Node.js leaderboard

--- sketch.js
+++ sketch.js
@@ -11,9 +11,7 @@ function setup() {
 function draw() {
     background(220);
     if (!isGameStarted) {
-        startButton = createButton('Start Game!');
-        startButton.position(gameWidth / 2, gameHeight /2);
-        startButton.mousePressed(startGame);
+        createWelcomeScreen();
     } else if (pong.getIsGameOver()) {
         fill(255, 0, 0);
         textSize(60);
@@ -35,4 +33,31 @@ function startGame() {
     startButton.hide();
     pong = new Pong(gameWidth, gameHeight, 5, 5, 3, 0);
     isGameStarted = true;
+}
+
+function createWelcomeScreen() {
+    fill(0);
+    textSize(60);
+    textAlign(CENTER);
+    text("Welcome to my game!", gameWidth / 2 - 175, gameHeight / 2 - 200, 350, 300);
+    createStartButton();
+}
+
+function createStartButton() {
+    //create start game button
+    fill(0, 128, 0);
+    //noStroke();
+    rect(gameWidth / 2 - 75, gameHeight/ 2 - 25, 150, 30);
+    fill(0);
+    textSize(24);
+    text("Start Game!", gameWidth / 2, gameHeight / 2);
+    textAlign(LEFT);
+}
+
+function mousePressed() {
+    if (mouseX > gameWidth / 2 - 75 && mouseX < gameWidth / 2 + 75
+        && mouseY > gameHeight / 2 - 25  && mouseY < gameHeight / 2 + 25) {
+            pong = new Pong(gameWidth, gameHeight, 5, 5,3, 0);
+            isGameStarted = true;
+        }
 }
